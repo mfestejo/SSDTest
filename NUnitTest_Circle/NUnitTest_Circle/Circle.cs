48c3b265779af4afaa26747878469c421b54766b
Miguel <mcfestejo@gmail.com>
2012-03-22 12:53:54 -0700

    second commit

---
+++ NUnitTest_Circle/NUnitTest_Circle/Circle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NUnitTest_Circle
+{
+    public class Circle
+    {
+        private decimal radius = 0;
+        private decimal area = 0;
+        private decimal circumference = 0;
+        public const decimal PI = 3.1415926M;
+        public decimal Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+        public Circle(decimal r)
+        {
+            Radius = r;
+        }
+        public decimal getArea()
+        {
+            area = PI * Radius * Radius;
+            return area;
+        }
+
+        public decimal getCircumference()
+        {
+            circumference = 2 * PI * Radius;
+            return circumference;
+        }
+
+        public decimal getRadius()
+        {
+            return Radius;
+        }
+        public void setRadius(decimal r)
+        {
+            Radius = r;
+        }
+    }
+}
---
+++ NUnitTest_Circle/NUnitTest_Circle/CircleTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+namespace NUnitTest_Circle
+{
+    [TestFixture]
+    public class CircleTest
+    {
+        [Test]
+        public void TestCircleArea()
+        {
+            Circle circle = new Circle(5);
+            decimal area = circle.getArea();
+            Assert.AreEqual(78.539815, area);
+        }
+
+        [Test]
+        public void TestCircleCircuference()
+        {
+            Circle circle = new Circle(5);
+            decimal circum = circle.getCircumference();
+            Assert.AreEqual(31.415926, circum);
+        }
+
+        [Test]
+        public void TestCircle2()
+        {
+            Circle circle = new Circle(5);
+            decimal circum = circle.getCircumference();
+            Assert.AreEqual(31.415926, circum);
+        }
+
+        [Test]
+        public void TestCircleRadiusSet()
+        {
+            Circle circle = new Circle(5);
+            circle.setRadius(6);
+            decimal rad = circle.getRadius();
+            Assert.AreEqual(6, rad);
+        }
+    }
+}
